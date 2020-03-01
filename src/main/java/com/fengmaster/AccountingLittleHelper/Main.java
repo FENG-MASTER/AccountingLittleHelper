0ee0893cc0e5398a907927548d093c89336611b2
FengMaster <554286238@qq.com>
2020-03-02 00:19:32 +0800

    Initial commit

---
+++ src/main/java/com/fengmaster/AccountingLittleHelper/Main.java
@@ -0,0 +1,11 @@
+package com.fengmaster.AccountingLittleHelper;
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+    }
+
+
+}
---
+++ src/main/java/com/fengmaster/AccountingLittleHelper/progress/IProgress.java
@@ -0,0 +1,9 @@
+package com.fengmaster.AccountingLittleHelper.progress;
+
+public interface IProgress {
+
+    String getName();
+
+    boolean progress(String[] args);
+
+}
